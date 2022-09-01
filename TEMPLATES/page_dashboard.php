e33972b4ffbf295cce9972282beb317c1026f14b
Bianca Carvalho <carvalho.bianca@hotmail.com>
2022-09-01 22:31:45 +0100

    modified class to be used in the page-dashboard and page-not-found style

---
+++ TEMPLATES/page_dashboard.php
@@ -0,0 +1,5 @@
+<div class="page messages-color ">
+   <h1 >Bem Vindo ao Dashboard</h1>
+   <div></div>
+   <p>Esta página só será exibida se o utilizador fizer uma autenticação bem sucedida.</p>
+</div>
--- TEMPLATES/page_not_found.php
+++ TEMPLATES/page_not_found.php
@@ -1,4 +1,4 @@
-<div class="page page-not-found">
+<div class="page messages-color">
     <h1>404 - Page Not Found</h1>
     <p>A págia solicidada não existe.</p>
 </div>
