dc8776bca549465ca4e9b4d5f97f304d6adc55f9
Bianca Carvalho <carvalho.bianca@hotmail.com>
2022-09-01 16:34:45 +0100

    login form style defined

---
+++ TEMPLATES/page_login.php
@@ -0,0 +1,19 @@
+<div>
+    <form action="?route=authenticated" method="POST" class="form">
+        <h2 class="text-align">Área Reservada</h2>
+        <div class="horizontal-line"></div>
+        <div class="form-wrapper">
+            <label for="username" class="animatedImput">
+            <input type="text" id="username" name="username" placeholder="Nome">
+            </label> 
+        </div>
+        <div class="form-wrapper">
+            <label for="password" class="animatedImput">
+            <input type="password" id="username" name="username" placeholder="Senha">
+            </label> 
+        </div>
+        <div class="form-wrapper">
+            <button>Login</button>
+        </div>
+    </form>
+</div>
